1047cb8113bd0eb4606cde811a2251e67253db4f
Maliakal T <maliakal.treasa@gmail.com>
2022-05-22 22:38:07 +0530

    Second Commit

--- Display Query ( 3 to 9).sql
+++ Display Query ( 3 to 9).sql
@@ -4,11 +4,21 @@ WHERE (cus_name like 'A%' or cus_name like '%A') ;
 #******************************
 
 #3) Display the total number of customers based on gender who have placed orders of worth at least Rs.3000.
-
-select * from `order` where ORD_AMOUNT > 3000;
-select count(CUS_GENDER) from customer group by cus_gender; 
-
-
+# complete 
+select count(cus_gender) as TotalNoOfCustomers, cus_gender 
+from 
+(
+select cus_gender, cus_name from customer as cus
+inner join
+(
+select ord_id, cus_id from `order` where ord_amount >= '3000'
+)
+as o
+on cus.cus_id = o.cus_id
+group by cus.cus_id
+)
+as T
+group by cus_gender;
 
 #******************************
 
@@ -34,7 +44,19 @@ on sup.supp_id = sp.SUPP_ID;
 #*********************************
 
 #6) Find the least expensive product from each category and print the table with category id, name, product name and price of the product
+#complete
 
+select cat.CAT_ID, CAT_NAME, PRO_ID, PRO_NAME, min(MIN_PRICE) from category as cat
+inner join
+(
+select prod.PRO_ID, PRO_NAME, CAT_ID, MIN_PRICE from product as prod
+inner join
+(select pro_id as PRO_ID, min(supp_price) as MIN_PRICE from supplier_pricing 
+group by pro_id) as sp
+on prod.pro_id = sp.pro_id)
+as pro_sp
+on cat.cat_id = pro_sp.CAT_ID
+group by cat_id;
 
 #***************************
 #7) Display the Id and Name of the Product ordered after “2021-10-05”.
